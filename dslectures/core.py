fafd47911188c06680ae1f55bac5eac9dba51b57
leandro <leandro.vonwerra@spoud.io>
2020-02-29 17:06:43 +0100

    chore: make data download naming consistent and check for existing paths

--- dslectures/core.py
+++ dslectures/core.py
@@ -14,20 +14,21 @@ def get_dataset(dataset_name: str):
     """
 
     name_to_id = {
-        "embeddings": "1dRwSXbFTcQbn8c3V24G92wFY4DXZ1SDt",
-        "imdb": "1wF0YEmQOwceJz2d6w4CfhBgydU87dPGl",
-        "housing": "1d7oOKdDmZFx8wf0c8OfuTW1FpUyJHABh",
-        "housing_gmaps": "1R1RUHAXxzrIngRJMFwyp4vZRVICd-I6T",
-        "housing_addresses": "1mOK0uyRz5Zs-Qo7mVMlxwtb2xn1E6N9Q",
+        "word2vec-google-news-300.pkl": "1dRwSXbFTcQbn8c3V24G92wFY4DXZ1SDt",
+        "imdb.csv": "1wF0YEmQOwceJz2d6w4CfhBgydU87dPGl",
+        "housing.csv": "1d7oOKdDmZFx8wf0c8OfuTW1FpUyJHABh",
+        "housing_gmaps_data_raw.csv": "1R1RUHAXxzrIngRJMFwyp4vZRVICd-I6T",
+        "housing_addresses.csv": "1mOK0uyRz5Zs-Qo7mVMlxwtb2xn1E6N9Q",
     }
 
+    path = '../data/'
+    gdrive_path = "https://docs.google.com/uc?export=download&id="
     if dataset_name in name_to_id:
+        if os.path.exists(path + dataset_name):
+            print(f"Dataset already exists at '{path + dataset_name}' and is not downloaded again.")
+            return
         try:
-            path = '../data/'
-            file_url = (
-                "https://docs.google.com/uc?export=download&id="
-                + name_to_id[dataset_name]
-            )
+            file_url =  gdrive_path + name_to_id[dataset_name]
             wget.download(file_url, out=path)
         except Exception as e:
             print("Something went wrong during download. Try again.")
